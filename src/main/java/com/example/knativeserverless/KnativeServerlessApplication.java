1ec888db6f377ac8f7752136457cf1616cadba1f
Sankarganesh Eswaran <sankarganesh.eswaran@idealo.de>
2019-10-11 12:19:19 +0200

    Sample Spring RestController

--- src/main/java/com/example/knativeserverless/KnativeServerlessApplication.java
+++ src/main/java/com/example/knativeserverless/KnativeServerlessApplication.java
@@ -1,13 +1,26 @@
 package com.example.knativeserverless;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class KnativeServerlessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KnativeServerlessApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KnativeServerlessApplication.class, args);
+    }
 
+    @Value("${TARGET:World}")
+    String target;
+
+    @RestController
+    class Controller {
+        @GetMapping("/")
+        String hello() {
+            return "Hello " + target + "!";
+        }
+    }
 }
