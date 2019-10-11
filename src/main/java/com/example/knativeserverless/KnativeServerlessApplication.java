6b5909afe26225fd7399157d1a40018a8ebaa37f
Sankarganesh Eswaran <sankarganesh.eswaran@idealo.de>
2019-10-11 12:14:03 +0200

    Sample Spring boot application

---
+++ src/main/java/com/example/knativeserverless/KnativeServerlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.knativeserverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnativeServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnativeServerlessApplication.class, args);
+	}
+
+}
